6cac22e9959e2e2ae63cdfadd247106915ebdc83
Luis Anguita <luis.anguita@solem.cl>
2022-02-02 19:31:15 -0300

    FIX:slack

--- gradle.groovy
+++ gradle.groovy
@@ -4,55 +4,57 @@
 	ejecucion.call()
 */
 def call(){
-    stage("Paso 1: Build && Test"){
-        sh "gradle clean build"
-    }
-    stage("Paso 2: Sonar - Análisis Estático"){
-        sh "echo 'Análisis Estático!'"
-        withSonarQubeEnv('SonarQube') {
-            sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+    stages{
+        stage("Paso 1: Build && Test"){
+            sh "gradle clean build"
         }
-    }
-    stage("Paso 3: Curl Springboot Gradle sleep 20"){
-        sh "gradle bootRun&"
-        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-    }
-    stage("Paso 4: Subir Nexus"){
-        nexusPublisher nexusInstanceId: 'nexus',
-        nexusRepositoryId: 'devopsusach',
-        packages: [
-            [$class: 'MavenPackage',
-                mavenAssetList: [
-                    [classifier: '',
-                    extension: '.jar',
-                    filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar'
-                ]
-            ],
-                mavenCoordinate: [
-                    artifactId: 'DevOpsUsach2020',
-                    groupId: 'com.devopsusach2020',
-                    packaging: 'jar',
-                    version: '0.0.1'
+        stage("Paso 2: Sonar - Análisis Estático"){
+            sh "echo 'Análisis Estático!'"
+            withSonarQubeEnv('SonarQube') {
+                sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+            }
+        }
+        stage("Paso 3: Curl Springboot Gradle sleep 20"){
+            sh "gradle bootRun&"
+            sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        }
+        stage("Paso 4: Subir Nexus"){
+            nexusPublisher nexusInstanceId: 'nexus',
+            nexusRepositoryId: 'devopsusach',
+            packages: [
+                [$class: 'MavenPackage',
+                    mavenAssetList: [
+                        [classifier: '',
+                        extension: '.jar',
+                        filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar'
+                    ]
+                ],
+                    mavenCoordinate: [
+                        artifactId: 'DevOpsUsach2020',
+                        groupId: 'com.devopsusach2020',
+                        packaging: 'jar',
+                        version: '0.0.1'
+                    ]
                 ]
             ]
-        ]
+        }
+        stage("Paso 5: Descargar Nexus"){
+            sh ' curl -X GET -u $user-nexus:$password-nexus "http://nexus:8081/repository/devopsusach/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+        }
+        stage("Paso 6: Levantar Artefacto Jar"){
+            sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+        }
+        stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
+            sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        }
     }
-    stage("Paso 5: Descargar Nexus"){
-        sh ' curl -X GET -u $user-nexus:$password-nexus "http://nexus:8081/repository/devopsusach/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
-    }
-    stage("Paso 6: Levantar Artefacto Jar"){
-        sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
-    }
-    stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
-        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-    }
-}
-post {
-    success {
-        slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
-    }
-    failure {
-        slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+    post {
+        success {
+            slackSend (color: '#00FF00', message: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+        }
+        failure {
+            slackSend (color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+        }
     }
 }
 return this;
